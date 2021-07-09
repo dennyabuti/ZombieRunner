fa9a2fa20aaf093fca88af768f71edcf709c8d65
Dennis Nyabuti <dennisnyabuti@Denniss-MacBook-Pro.local>
2021-07-09 17:58:47 -0600

    checkpoint

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,9 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera fPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -17,9 +20,35 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        RaycastHit hit;
-        Physics.Raycast(fPCamera.transform.position, fPCamera.transform.forward, out hit, range);
+        PlayMuzzleFlash();
+        ProcessRayCast();
+    }
 
-        Debug.Log($"Shot {hit.transform.name}");
+    void ProcessRayCast()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fPCamera.transform.position, fPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpact(hit);
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+
+            if (target == null) return;
+
+            target.TakeDamage(damage);
+        }
+    }
+
+    void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+        Destroy(impact, .1f);
     }
 }
